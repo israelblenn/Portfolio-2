15e5626cf47f8ce7d25fd0b587ce4ee5caf25723
Israel <israelblenn@gmail.com>
2026-03-08 13:07:04 +1100

    changing content

--- scripts/load-content.js
+++ scripts/load-content.js
@@ -101,7 +101,7 @@
         requestSync();
     }
 
-    fetch('content.json')
+    fetch('content.json?v=' + Date.now())
         .then(function(response) {
             if (!response.ok) {
                 throw new Error('Failed to load content.json');
